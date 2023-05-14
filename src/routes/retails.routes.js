ce6cbf026907a51fa4981b5912d9d62f6920cab8
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-05-14 18:38:03 -0300

    fix: reailts import

--- src/routes/retails.routes.js
+++ src/routes/retails.routes.js
@@ -1,9 +1,8 @@
 import {Router} from "express"
-import { getRetails,postRetails } from "../controllers/retails.controller.js"
+import { getRetails } from "../controllers/retails.controller.js"
 
 const retailsRouter = Router()
 
 retailsRouter.get("/retails", getRetails)
-retailsRouter.post("/retails",postRetails)
 
 export default retailsRouter

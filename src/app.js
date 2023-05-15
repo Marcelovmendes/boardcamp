fc75afb107f2c1d07fddff1aad74c17471b19f46
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-05-15 10:09:22 -0300

    fix: settings deploy

--- src/app.js
+++ src/app.js
@@ -15,4 +15,4 @@ app.use(customersRouter)
 app.use(retailsRouter)
 
 const port = process.env.PORT || 5000
-app.listen(port, ()=> console.log("Server is running on port " + PORT))
+app.listen(port, ()=> console.log("Server is running on port " + port))

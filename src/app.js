1153484d891fd946ac7ac600eb1e85f7ba235ead
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-05-15 10:01:55 -0300

    feat: settings deploy

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ import cors from "cors"
 import gamesRouter from "./routes/game.routes.js"
 import customersRouter from "./routes/customers.routes.js"
 import retailsRouter from "./routes/retails.routes.js"
+import dotenv from "dotenv"
 
+dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(gamesRouter)    
+app.use(gamesRouter)        
 app.use(customersRouter)
 app.use(retailsRouter)
 
-const PORT = 5000
-app.listen(PORT, ()=> console.log("Server is running on port " + PORT))
+const port = process.env.PORT || 5000
+app.listen(port, ()=> console.log("Server is running on port " + PORT))
